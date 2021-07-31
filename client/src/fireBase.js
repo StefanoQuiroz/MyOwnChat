b64905dbe66ff1a3bfcbfd6842411889d786ecf0
Stefano <piero.quiroz93@gmail.com>
2021-07-30 19:32:32 -0500

    Modified files and add firebase authentication on Client

---
+++ client/src/fireBase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCllyEDqm4JvZd6FYxWJIlc4Ag8gz1I-5k",
+    authDomain: "whatsappmern-34253.firebaseapp.com",
+    projectId: "whatsappmern-34253",
+    storageBucket: "whatsappmern-34253.appspot.com",
+    messagingSenderId: "79285029494",
+    appId: "1:79285029494:web:b8beb3c63a62159c47ed57"
+  };
+
+   
