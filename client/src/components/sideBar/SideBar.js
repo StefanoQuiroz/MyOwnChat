da6b382e0f41383534e11c369870d5c16f4d4469
Stefano <piero.quiroz93@gmail.com>
2021-07-30 23:49:32 -0500

    Last changes of the day

--- client/src/components/sideBar/SideBar.js
+++ client/src/components/sideBar/SideBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from './SideBar.module.scss';
+import { MdDonutLarge } from "react-icons/md";
 const SideBar = () => {
     return (
         <div className={styles.sideBar}>
-            
+            <div className={styles.sideBarHeader}>
+                <div className={styles.sideBarHeaderRight}>
+                    <MdDonutLarge/>
+                </div>
+                <div className={styles.sideBarHeaderLeft}>
+
+                </div>
+                
+            </div>
         </div>
     );
 }
