35c377b94e6a0629c776bf637c02f680bd023b93
Stefano <piero.quiroz93@gmail.com>
2021-08-01 14:00:34 -0500

    Side Bar tags and styles finished

--- client/src/components/sideBar/SideBar.js
+++ client/src/components/sideBar/SideBar.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import styles from './SideBar.module.scss';
-import { MdDonutLarge } from "react-icons/md";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import SideBarChat from '../sideBarChat/SideBarChat';
+
 const SideBar = () => {
     return (
         <div className={styles.sideBar}>
             <div className={styles.sideBarHeader}>
+                <Avatar />
                 <div className={styles.sideBarHeaderRight}>
-                    <MdDonutLarge/>
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
                 </div>
-                <div className={styles.sideBarHeaderLeft}>
+            </div>
+            
+            <div className={styles.sideBarSearch}>
+                <div className={styles.sideBarSearchContainer}>
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder="Search or start new chat" />
+                </div>
+            </div>
 
-                </div>
-                
+            <div className={styles.sideBarChats}>
+                <SideBarChat/>
+                <SideBarChat/>
+                <SideBarChat/>
             </div>
         </div>
     );
