7c4554147a327ee0a5bee6c988e3e35be393f0bd
Stefano <piero.quiroz93@gmail.com>
2021-08-01 17:19:43 -0500

    Add tags for the chat section

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,9 +1,63 @@
 import React from 'react';
 import styles from './Chat.module.scss'; 
+import { Avatar, IconButton } from '@material-ui/core';
+import { SearchOutlined, AttachFile, MoreVert } from '@material-ui/icons';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';  
+import MicIcon from '@material-ui/icons/Mic'
 const Chat = () => {
     return (
         <div className={styles.chat}>
-            
+            <div className={styles.chatHeader}>
+                <Avatar/>
+                <div className={styles.chatHeaderInfo}>
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className={styles.chatHeaderRight}>
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className={styles.chatBody}>
+                <p className={styles.chatMessage}>
+                    <span className={styles.chatName}>Coding Dojo</span>
+                    This is a message
+                    <span className={styles.chatTimeStamp}>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className={styles.chatReciever}>
+                    <span className={styles.chatName}>Stefano</span>
+                    This is a message
+                    <span className={styles.chatTimeStamp}>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className={styles.chatMessage}>
+                    <span className={styles.chatName}>Coding Dojo</span>
+                    This is a message
+                    <span className={styles.chatTimeStamp}>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>  
+
+            <div className={styles.chatFooter}>
+                <InsertEmoticonIcon/>
+                <form>
+                    <input type="text" placeholder="Type a message"/>
+                    <button type="submit">Send a message</button>
+                </form>
+                <MicIcon/>
+            </div>          
         </div>
     );
 }
