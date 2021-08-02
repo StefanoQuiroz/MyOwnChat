c1d70d2eb2ddb02d07409280e5207e216469a5dc
Stefano <piero.quiroz93@gmail.com>
2021-08-01 21:07:34 -0500

    Modified files on create and findAllMessages

--- server/controllers/whatsApp.controller.js
+++ server/controllers/whatsApp.controller.js
@@ -1,19 +1,47 @@
 const WhatsApp = require('../models/whatsApp.model');
 
-const findAll = (req, res) => {
-    res.status(200).status("Hello Coding Dojo");
+const prove = (req, res) => {
+    res.status(200).send("Good Job!!");
+}
+
+const findAll =  (req, res) => {
+    WhatsApp.find()
+        .then(response => {
+            res.json({data: response});
+            res.status(200);
+        })
+        .catch(err => {
+            res.json({error: err});
+            res.status(500);
+        })
+    /* WhatsApp.find((err, data) => {
+        if(err){
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    }) */
 }
 
 const createMessage = (req, res) => {
-    WhatsApp.create(req.body, (err, data) => {
+    WhatsApp.create(req.body)
+        .then(response => {
+            res.json({data: response});
+            res.status(200);
+        })
+        .catch(err => {
+            res.json({error: err});
+            res.status(500);
+        })
+    /* WhatsApp.create(req.body, (err, data) => {
         if(err){
             res.status(500).send(err);
         }
         else{
             res.status(201).send(`New Message created: \n ${data}`);
         }
-    })
+    }) */
 }
 
 
-module.exports = { findAll, createMessage };
+module.exports = { prove, findAll, createMessage };
