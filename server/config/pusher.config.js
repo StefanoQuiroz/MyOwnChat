f9017d7e65391f14aa85a0e1487bfaf190d2e04e
Stefano <piero.quiroz93@gmail.com>
2021-08-03 16:57:30 -0500

    Pusher config

--- server/config/pusher.config.js
+++ server/config/pusher.config.js
@@ -1,5 +1,5 @@
 const Pusher = require('pusher');
-
+const mongoose = require('mongoose');
 const pusher = new Pusher({
     appId: "1243776",
     key: "dad86e5da3fa7bc1b1d8",
@@ -8,4 +8,24 @@ const pusher = new Pusher({
     useTLS: true
 });
 
-module.exports = pusher;
+const bd = mongoose.connection;
+bd.once('open', ()=>{
+    console.log("DB connected");
+    const msgCollection = bd.collection('messages');
+    const changeStream = msgCollection.watch();
+
+    changeStream.on("change", (change) => {
+        console.log("A change occured",change);
+        if(change.operationType === "insert"){
+            const messageDetails = change.fullDocument;
+            pusher.trigger("messages", "inserted", {
+                name: messageDetails.name,
+                message: messageDetails.message
+            });
+        } else {
+            console.log("Error triggering Pusher");
+        }
+    })
+})
+
+module.exports = bd;
